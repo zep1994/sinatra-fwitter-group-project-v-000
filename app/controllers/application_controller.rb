d2a490b7a21cac70585dc35d5d95f7a6de70c65a
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-09-10 16:28:41 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -61,4 +61,16 @@ class ApplicationController < Sinatra::Base
   end
 
 
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
+
+
 end
