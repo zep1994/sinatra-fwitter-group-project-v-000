85ccfae30b121bd975cc415d16093d4854203327
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-09-10 16:56:08 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :tweets
 
   validates :username, length: { in: 5..10 }
-  validates :email, presence: true, uniquencess: true
+  validates :email, presence: true, uniqueness: true
   has_secure_password
 
   before_save do
