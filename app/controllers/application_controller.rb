cea6ebac3a20d4c77730652e11fc28afba9df432
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-09-10 18:50:33 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,12 @@
 require './config/environment'
-
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
-
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "password_security_rocks"
+    set :session_secret, "password_security_is_important"
   end
-
   get '/' do
     if logged_in?
       redirect "/users/#{current_user.slug}"
@@ -17,7 +14,6 @@ class ApplicationController < Sinatra::Base
       erb :index
     end
   end
-
   get '/signup' do
     if !logged_in?
       erb :signup
@@ -25,7 +21,6 @@ class ApplicationController < Sinatra::Base
       redirect '/tweets'
     end
   end
-
   post '/signup' do
     if !params[:username].empty? && !params[:password].empty? && !params[:email].empty?
       user = User.create(username: params[:username], password: params[:password], email: params[:email])
@@ -39,7 +34,6 @@ class ApplicationController < Sinatra::Base
       redirect '/signup'
     end
   end
-
   get '/login' do
     if !logged_in?
       erb :login
@@ -47,37 +41,31 @@ class ApplicationController < Sinatra::Base
       redirect '/tweets'
     end
   end
-
   post '/login' do
     if !params[:username].empty? && !params[:password].empty?
       user = User.find_by(username: params[:username])
-      if user && user.authenticate(params[:username])
+      if user && user.authenticate(params[:password])
         session[:user_id] = user.id
         redirect '/tweets'
       else
         redirect '/login'
-    end
-  else
-    redirect '/login'
+      end
+    else
+      redirect '/login'
     end
   end
-
   get '/logout' do
     if logged_in?
       session.delete("user_id")
     end
     redirect '/login'
   end
-
   helpers do
     def logged_in?
       !!session[:user_id]
     end
-
     def current_user
       User.find(session[:user_id])
     end
   end
-
-
 end
