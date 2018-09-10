c959a4ce64e74721dae01f4ecfee02a246601f98
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-09-10 18:59:04 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
+
   has_many :tweets
+
   validates :username, length: { in: 5..18 }
   validates :email, presence: true, uniqueness: true
   has_secure_password
+  
   before_save do
     self.slug = self.username.gsub(/[^a-zA-Z0-9]+/, "-").downcase
   end
