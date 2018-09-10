2d154f97de7c54d80bea007cdf0b589c38ed5265
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-09-10 18:38:07 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,11 @@
 
 ActiveRecord::Schema.define(version: 20180910165924) do
 
+  create_table "tweets", force: :cascade do |t|
+    t.string  "content"
+    t.integer "user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
