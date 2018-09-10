5aeb563af0a1eb8b1f2052d0d60bc9a7e45b48d3
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-09-10 16:06:45 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,38 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security_rocks"
+  end
+
+  get '/' do
+    if logged_in?
+      redirect "/users/#{current_user.slug}"
+    else
+      erb :index
+    end
+  end
+
+  get '/signup' do
+    if !logged_in?
+      erb :signup
+    else
+      redirect '/tweets'
+    end
+  end
+
+  post '/signup' do
+    if !params[:username].empty? && !params[:password].empty? && !params[:email].empty?
+      user = User.create(username: params[:username], password: params[:password], email: params[:email])
+      if user
+        session[:user_id] = user.id
+        redirect '/tweets'
+      else
+        redirect 'signup'
+      end
+    else
+      redirect 'signup'
+    end
   end
 
 end
