6573ff7176465edf2fccffa2607a71f9836f8f46
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-09-10 16:46:07 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
-  
+  get '/users/:slug' do
+    @user = User.find_by(slug: params[:slug])
+    if @user
+      erb :"/users/show"
+    else
+      redirect '/'
+    end
+  end
 
 end
