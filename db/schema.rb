016de1e7691cdfd6fe40c0b5cdf9df726a9e360b
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-09-10 17:41:17 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180909210330) do
+ActiveRecord::Schema.define(version: 20180910165924) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.string "password_digest"
+    t.string "slug"
   end
 
 end
